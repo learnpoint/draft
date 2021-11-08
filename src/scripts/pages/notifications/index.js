9435b6f7a4eb945e6c02e436e10e678788834a9d
fridabrodin <frida.brodin@hotmail.com>
2021-11-08 15:14:01 +0100

    Grouping design experiments

--- src/scripts/pages/notifications/index.js
+++ src/scripts/pages/notifications/index.js
@@ -1,30 +1,3 @@
-console.log("Hello!");
-
-// var acc = document.getElementsByClassName("expandable-button");
-var acc = document.getElementsByClassName("expandable-btn");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    const panel = document.getElementById("expandable-panel");
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-
-document.addEventListener("click", function() {
-  console.log("Document is clicked");
-});
-
 const togglers = Array.from(document.querySelectorAll('[data-element="expand-btn"]'));
 
 document.addEventListener('click', (ev) => {
