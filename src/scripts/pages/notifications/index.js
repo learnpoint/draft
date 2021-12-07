e1795c60f310bcf4787e61d939d240b1d1cc418e
Dhanfia <dhanfiarahman@gmail.com>
2021-12-07 10:14:23 +0100

    Mark all as read

--- src/scripts/pages/notifications/index.js
+++ src/scripts/pages/notifications/index.js
@@ -3,11 +3,18 @@ document.addEventListener(render.ready, () => {
     const togglers = Array.from(document.querySelectorAll('[data-element="expand-btn"]'));
     const collapsebuttons = Array.from(document.querySelectorAll('[data-element="collapse-btn"]'));
     const moveButtons = document.querySelectorAll('[data-element="move-btn"]');
+    const markAllAsReadButtun = document.querySelector('[data-element="mark-all-as-read-btn"]');
     const newList = document.getElementById("new");
     const previousList = document.getElementById("previous");
 
     document.addEventListener('click', (ev) => {
+
         const elm = ev.target;
+
+        if(elm == markAllAsReadButtun) {
+            markAllAsRead();
+        }
+        
         if (togglers.includes(elm)) {
             const selector = elm.getAttribute('data-target');
             toggle(selector);
@@ -28,7 +35,7 @@ document.addEventListener(render.ready, () => {
         const target = document.getElementById(selector);
         target.classList.remove('EXPAND');
     }
-  
+
     function moveItem(e) {
         console.log(e.target);
         e.target.innerText == "Mark as read" ? e.target.innerText = 'Mark as unread' : e.target.innerText = "Mark as read";
@@ -36,6 +43,16 @@ document.addEventListener(render.ready, () => {
         let itemTobeMoved = this.parentElement.parentElement.parentElement;
         console.log(moveTo);
         moveTo.prepend(itemTobeMoved);
+    }
+
+    function markAllAsRead() {
+        const listItems = newList.querySelectorAll('.notification');
+        console.log(listItems);
+        listItems.forEach(li => {
+            console.log( li.querySelector('[data-element="move-btn"] > span'));
+            li.querySelector('[data-element="move-btn"] > span').innerText = "Mark as unread";
+            previousList.prepend(li);
+        });
     }
 
     for (var i = 0; i < moveButtons.length; i++) {
