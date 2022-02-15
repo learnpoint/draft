2a05962f2060e52a1177c6a42f872bbe8061fe41
Dhanfia <dhanfiarahman@gmail.com>
2022-02-15 15:28:38 +0100

    Experimenting keypress focus trap more actions menu

--- src/scripts/core/expandable.js
+++ src/scripts/core/expandable.js
@@ -1,7 +1,13 @@
 document.addEventListener('click', event => {
 
-    const toggler = event.target;
-    if (!toggler.matches('[data-element="expandable.toggle-button"]')) {
+    const toggleButton = event.target.closest('[data-element="expandable.toggle-button"]');
+    if (!toggleButton) {
+        collapseAll();
+        return;
+    }
+
+    const collapseButton = event.target.closest('[data-element="expandable.collapse-button"]');
+    if (collapseButton) {
         collapseAll();
         return;
     }
@@ -13,15 +19,45 @@ document.addEventListener('click', event => {
 
     // if link or button:
     event.preventDefault();
-    toggler.setAttribute('aria-expanded', toggler.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
-    expandable.classList.toggle('EXPANDED');
+    toggleButton.setAttribute('aria-expanded', toggleButton.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
+    expandable.classList.toggle('EXPANDED');  
+    const expandedMenu = expandable.querySelector('[data-element="expandable.menu"]');
+    expandedMenu.focus();
+    document.addEventListener('keydown',TrapFocusOnExpandable);  
   
 });
+
+
+
+function TrapFocusOnExpandable(e){
+
+  const focusableElements = e.target.matches('[data-element="expandable.menu"]') ? e.target.querySelectorAll(`button`) :  e.target.closest('[data-element="expandable.menu"]').querySelectorAll(`button`) ;
+
+  const focusableElementsArray = [...focusableElements]; 
+  console.log(focusableElementsArray);
+
+  const index = focusableElementsArray.indexOf(document.activeElement); 
+  let nextIndex = 0;
+
+  if (e.keyCode === 38) {
+    // up arrow
+    e.preventDefault();
+    nextIndex= index > 0 ? index-1 : 0;
+    focusableElementsArray[nextIndex].focus();
+  }
+  else if (e.keyCode === 40) {
+    // down arrow
+    e.preventDefault();
+    nextIndex= index+1 < focusableElementsArray.length ? index+1 : index;
+    focusableElementsArray[nextIndex].focus();
+  }
+}
 
 
 function collapseAll() {
     const expandables = document.querySelectorAll('[data-component="expandable"]');
     expandables.forEach(expandable => {
         expandable.classList.remove('EXPANDED');
+        expandable.querySelector('[data-element="expandable.toggle-button"]').setAttribute('aria-expanded', "false");
     });
 }
